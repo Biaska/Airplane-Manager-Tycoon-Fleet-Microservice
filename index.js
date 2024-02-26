300999cca19abd6e0f8c6bc66d10a5c889b4adf8
biaska <biaska70@gmail.com>
2024-02-26 14:12:13 -0800

    cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 dotenv.config();
